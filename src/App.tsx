1658d9d6a635727ccda916f3f318b17ac9f796bf
hhx <936446507@qq.com>
2020-11-03 18:21:01 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ import routes from './routes';
 import logo from 'src/assets/img/logo.png';
 function App() {
   return (
-    <div className='App' style={{ textAlign: 'center' }}>
+    <div className='App' style={{ textAlign: 'center',color: '#fff' }}>
       <img src={logo} alt='logo' />
-      <p style={{ 'fontSize': '18px', 'fontWeight': 600, 'marginBottom': '20px' }}>
+      <p style={{ 'fontSize': '18px','fontWeight': 600, 'marginBottom': '20px' }}>
         Welcome to Your React.js App
       </p>
       {{#if router}}
