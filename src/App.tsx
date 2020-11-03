7caeb11da52df2c76e61d8592e4a2b2abe1a2d2d
hhx <936446507@qq.com>
2020-11-03 18:14:43 +0800

    test style

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='App' style={{ textAlign: 'center' }}>
       <img src={logo} alt='logo' />
-      <p style={{ fontSize: '18px', fontWeight: 600, marginBottom: '20px' }}>
+      <p style={{ 'fontSize': '18px', 'fontWeight': 600, 'marginBottom': '20px' }}>
         Welcome to Your React.js App
       </p>
       {{#if router}}
